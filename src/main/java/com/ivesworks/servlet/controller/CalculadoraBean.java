391a46d4415a30185c0895d220d18e41e3c910aa
ivesmendes <ivesmedes@gmail.com>
2024-04-09 12:21:48 -0300

    funções subtrair, multiplicar e dividir e seus botões respectivos

--- src/main/java/com/ivesworks/servlet/controller/CalculadoraBean.java
+++ src/main/java/com/ivesworks/servlet/controller/CalculadoraBean.java
@@ -1,34 +1,138 @@
 package com.ivesworks.servlet.controller;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
-public class CalculadoraBean {
-
-	private Double valorA;
-	private Double valorB;
-	private Double resultado;
+public class CalculadoraBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Double valorSomaA;
+	private Double valorSomaB;
+	private Double valorSubA;
+	private Double valorSubB;
+	private Double valorMultA;
+	private Double valorMultB;
+	private Double valorDivA;
+	private Double valorDivB;
+	private Double resultadoSoma;
+	private Double resultadoSub;
+	private Double resultadoMult;
+	private Double resultadoDiv;
+	
 	public void somar() {
-	this.resultado = this.valorA + this.valorB;
+		this.resultadoSoma = this.valorSomaA + this.valorSomaB;
 	}
 	
-	public Double getValorA() {
-		return valorA;
+	public void subtrair() {
+		this.resultadoSub = this.valorSubA - this.valorSubB;
 	}
-	public void setValorA(Double valorA) {
-		this.valorA = valorA;
+	
+	public void multiplicar() {
+		this.resultadoMult = this.valorMultA * this.valorMultB;
 	}
-	public Double getValorB() {
-		return valorB;
+	
+	public void dividir() {
+		this.resultadoDiv = this.valorDivA / this.valorDivB;
 	}
-	public void setValorB(Double valorB) {
-		this.valorB = valorB;
+	
+	public Double getResultadoSub() {
+		return resultadoSub;
 	}
-	public Double getResultado() {
-		return resultado;
+
+	public void setResultadoSub(Double resultadoSub) {
+		this.resultadoSub = resultadoSub;
 	}
-	public void setResultado(Double resultado) {
-		this.resultado = resultado;
+
+	public Double getResultadoMult() {
+		return resultadoMult;
 	}
+
+	public void setResultadoMult(Double resultadoMult) {
+		this.resultadoMult = resultadoMult;
+	}
+
+	public Double getResultadoDiv() {
+		return resultadoDiv;
+	}
+
+	public void setResultadoDiv(Double resultadoDiv) {
+		this.resultadoDiv = resultadoDiv;
+	}
+
+	public Double getResultadoSoma() {
+		return resultadoSoma;
+	}
+	public void setResultadoSoma(Double resultadoSoma) {
+		this.resultadoSoma = resultadoSoma;
+	}
+
+	public Double getValorSomaA() {
+		return valorSomaA;
+	}
+
+	public void setValorSomaA(Double valorSomaA) {
+		this.valorSomaA = valorSomaA;
+	}
+
+	public Double getValorSomaB() {
+		return valorSomaB;
+	}
+
+	public void setValorSomaB(Double valorSomaB) {
+		this.valorSomaB = valorSomaB;
+	}
+
+	public Double getValorSubA() {
+		return valorSubA;
+	}
+
+	public void setValorSubA(Double valorSubA) {
+		this.valorSubA = valorSubA;
+	}
+
+	public Double getValorSubB() {
+		return valorSubB;
+	}
+
+	public void setValorSubB(Double valorSubB) {
+		this.valorSubB = valorSubB;
+	}
+
+	public Double getValorMultA() {
+		return valorMultA;
+	}
+
+	public void setValorMultA(Double valorMultA) {
+		this.valorMultA = valorMultA;
+	}
+
+	public Double getValorMultB() {
+		return valorMultB;
+	}
+
+	public void setValorMultB(Double valorMultB) {
+		this.valorMultB = valorMultB;
+	}
+
+	public Double getValorDivA() {
+		return valorDivA;
+	}
+
+	public void setValorDivA(Double valorDivA) {
+		this.valorDivA = valorDivA;
+	}
+
+	public Double getValorDivB() {
+		return valorDivB;
+	}
+
+	public void setValorDivB(Double valorDivB) {
+		this.valorDivB = valorDivB;
+	}
+	
+	
 	
 }
